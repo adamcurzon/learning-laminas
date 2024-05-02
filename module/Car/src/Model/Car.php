be64596f83826ab85ec289b29cce6e6ab7aab8f4
acurzon <adam@curzon.org>
2024-05-02 15:26:24 +0100

    feat: add car model

---
+++ module/Car/src/Model/Car.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Car\Model;
+
+use DomainException;
+use Laminas\Filter\ToInt;
+use Laminas\Filter\StripTags;
+use Laminas\Filter\StringTrim;
+use Laminas\Validator\StringLength;
+use Laminas\InputFilter\InputFilter;
+use Laminas\InputFilter\InputFilterInterface;
+use Laminas\InputFilter\InputFilterAwareInterface;
+
+class Car implements InputFilterAwareInterface
+{
+    public $id;
+    public $name;
+    public $model;
+
+    private $inputFilter;
+
+    public function exchangeArray(array $array): void
+    {
+        $this->id     = !empty($array['id']) ? $array['id'] : null;
+        $this->name = !empty($array['name']) ? $array['name'] : null;
+        $this->model  = !empty($array['model']) ? $array['model'] : null;
+    }
+
+    public function setInputFilter(InputFilterInterface $inputFilter)
+    {
+        throw new DomainException(sprintf(
+            '%s does not allow injection of an alternate input filter',
+            __CLASS__
+        ));
+    }
+
+    public function getInputFilter()
+    {
+        if ($this->inputFilter) {
+            return $this->inputFilter;
+        }
+
+        $inputFilter = new InputFilter();
+
+        $inputFilter->add([
+            'name' => 'id',
+            'required' => true,
+            'filters' => [
+                ['name' => ToInt::class],
+            ],
+        ]);
+
+        $inputFilter->add([
+            'name' => 'name',
+            'required' => true,
+            'filters' => [
+                ['name' => StripTags::class],
+                ['name' => StringTrim::class],
+            ],
+            'validators' => [
+                [
+                    'name' => StringLength::class,
+                    'options' => [
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 100,
+                    ],
+                ],
+            ],
+        ]);
+
+        $inputFilter->add([
+            'name' => 'model',
+            'required' => true,
+            'filters' => [
+                ['name' => StripTags::class],
+                ['name' => StringTrim::class],
+            ],
+            'validators' => [
+                [
+                    'name' => StringLength::class,
+                    'options' => [
+                        'encoding' => 'UTF-8',
+                        'min' => 1,
+                        'max' => 100,
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->inputFilter = $inputFilter;
+        return $this->inputFilter;
+    }
+
+    public function getArrayCopy()
+    {
+        return [
+            'id'     => $this->id,
+            'name' => $this->name,
+            'model'  => $this->model,
+        ];
+    }
+}
