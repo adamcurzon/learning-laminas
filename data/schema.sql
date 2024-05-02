c25cef3ba6963d8a6ee04888739c2f841f4f574f
acurzon <adam@curzon.org>
2024-05-02 15:25:33 +0100

    feat: add sqlite DB connection

---
+++ data/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE car (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, `name` varchar(100) NOT NULL, model varchar(100) NOT NULL
+);
+
+INSERT INTO car (`name`, model) VALUES ('Ford Fiesta', 'Zetec');
+
+INSERT INTO car (`name`, model) VALUES ('BMW 1 Series', 'M Sport');
+
+INSERT INTO car (`name`, model) VALUES ('Pegeout 108', 'Base');
